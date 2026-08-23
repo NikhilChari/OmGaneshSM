015e2bd387b9492f92fe1861b16978e4d02e8a41
Nikhil <nikhilchari2005@gmail.com>
2026-08-23 22:53:22 +0530

    feat: add about achievements

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,6 +1,7 @@
 import AboutHero from '@/components/about/AboutHero'
 import HistorySection from '@/components/about/HistorySection'
 import MissionVisionSection from '@/components/about/MissionVisionSection'
+import AchievementsSection from '@/components/about/AchievementsSection'
 
 function About() {
   return (
@@ -8,6 +9,7 @@ function About() {
       <AboutHero />
       <HistorySection />
       <MissionVisionSection />
+      <AchievementsSection />
     </>
   )
 }
