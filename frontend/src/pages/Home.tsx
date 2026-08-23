51d7751450db347dc862f97b292d91a01982cc63
Nikhil <nikhilchari2005@gmail.com>
2026-08-23 13:21:56 +0530

    feat: add homepage upcoming events section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/components/home/HeroSection'
 import WelcomeSection from '@/components/home/WelcomeSection'
 import IntroductionSection from '@/components/home/IntroductionSection'
+import UpcomingEventsSection from '@/components/home/UpcomingEventsSection'
 function Home() {
   return (
     <>
       <HeroSection />
       <WelcomeSection />
       <IntroductionSection />
+      <UpcomingEventsSection />
     </>
   )
 }
